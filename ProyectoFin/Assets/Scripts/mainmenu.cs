2ea3c8c22664f662e5d61f742cbe7986d8cbdaf3
LastSoul <bmsa753@gmail.com>
2019-12-09 07:49:21 -0500

    Menu y solved import problem

---
+++ ProyectoFin/Assets/Scripts/mainmenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//scene changer
+    }
+    public void QuitGame() {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
