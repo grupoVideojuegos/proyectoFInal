8156470dcac6d44a97911217bea563c73b218873
Carlos Salazar <carfesal@espol.edu.ec>
2019-12-15 23:20:39 -0500

    Caida de bloque y animacion de agua (hay que arreglar)

---
+++ ProyectoFin/Assets/Scripts/CaidaBloque.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaidaBloque : MonoBehaviour
+{
+    [SerializeField] Transform destino;
+    [SerializeField] float velocidadCaida;
+
+    //variables a guardar
+    private Vector3 inicioBloque, finBloque; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(destino != null)
+        {
+            destino.parent = null;
+            inicioBloque = transform.position;
+            finBloque = destino.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if(destino != null)
+        {
+            float deltaVelocidad = velocidadCaida * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, destino.position, deltaVelocidad);
+        }
+        if(transform.position == destino.position)
+        {
+            destino.position = (destino.position == inicioBloque) ? finBloque : inicioBloque;
+        }
+    }
+}
---
+++ ProyectoFin/Assets/Scripts/Trayectoria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trayectoria : MonoBehaviour
+{
+    [SerializeField] Transform salida;
+    [SerializeField] Transform llegada;
+
+    private void OnDrawGizmosSelected()
+    {
+        if(salida != null && llegada != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(salida.position, llegada.position);
+            Gizmos.DrawSphere(salida.position, 0.15f);
+            Gizmos.DrawSphere(llegada.position, 0.15f);
+        }
+    }
+
+}
