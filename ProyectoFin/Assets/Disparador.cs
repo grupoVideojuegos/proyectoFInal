1b59e38a90e4266d5604b34d6ccb78b4f5532019
Carlos Salazar <carfesal@espol.edu.ec>
2019-12-23 13:34:39 -0500

    Colocacion de arma que dispara segun la posicion del jugador

---
+++ ProyectoFin/Assets/Disparador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparador : MonoBehaviour
+{
+
+    private float tiempoDisparo;
+    [SerializeField] float iniciarTiempoDisparo;
+    [SerializeField] GameObject bala;
+    private Transform jugador; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugador = GameObject.FindObjectOfType<Jugador>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tiempoDisparo <= 0)
+        {
+            Instantiate(bala, transform.position, Quaternion.identity);
+            tiempoDisparo = iniciarTiempoDisparo;
+        }
+        else
+        {
+            tiempoDisparo -= Time.deltaTime;
+        }
+    }
+}
--- ProyectoFin/Assets/Scripts/Bolita.cs
+++ ProyectoFin/Assets/Scripts/Bolita.cs
@@ -5,61 +5,29 @@ using UnityEngine;
 
 public class Bolita : MonoBehaviour
 {
-    [SerializeField] AudioClip[] sonidos;
-    
-    [SerializeField] float speedX = 8f;
-    [SerializeField] float speedY= 5f;
-    //estados
-    private Boolean inicio = false;
-    private Rigidbody2D rb;
-    
+    [SerializeField] float velocidad;
+    private Transform jugador;
+    private Vector2 target;
 
-    //Componentes guardados en referencias
-    AudioSource miAudio;
-   
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-        miAudio = GetComponent<AudioSource>();
-        rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.left*1, ForceMode2D.Impulse);
-
+        jugador = GameObject.FindObjectOfType<Jugador>().transform;
+        target = new Vector2(jugador.position.x, jugador.position.y);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (!inicio)//para verificar que la bolita se quede pegada a la platafor y no vuelva a pegarse en cada update
+        transform.position = Vector2.MoveTowards(transform.position,target,velocidad*Time.deltaTime);
+        if(transform.position.x == target.x && transform.position.y == target.y)
         {
-            
-            LanzarAlClick();
-        }
+            DestruirBala();
+        } 
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         
     }
-
-    private void LanzarAlClick()
+    void DestruirBala()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            inicio = true;
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(speedX,speedY); // accediendo a la velocidad del componente rigidbody
-            
-        }
-        
-    }
-
-    
-    //para hacer que se ejecute el sonido al rebotar la bola
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        //PARA QUE SOLO SE ESCUCHE EL SONIDO CUANDO YA HAYA INICIADO EL JUEGO 
-        if (inicio)
-        {
-            //elegir un sonido random cada vez que entre en colision
-            AudioClip sonido = sonidos[UnityEngine.Random.Range(0, sonidos.Length)];
-            miAudio.PlayOneShot(sonido);
-        }
-        
+        Destroy(gameObject);
     }
 }
