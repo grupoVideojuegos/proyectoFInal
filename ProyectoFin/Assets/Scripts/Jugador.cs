c50cbbc2a916b9e3a717fcb77c35c6f68a0bf24b
LastSoul <bmsa753@gmail.com>
2020-01-04 23:21:17 -0500

    mejoras en movimiento jugador + add al prefab

--- ProyectoFin/Assets/Scripts/Jugador.cs
+++ ProyectoFin/Assets/Scripts/Jugador.cs
@@ -213,5 +213,7 @@ public class Jugador : MonoBehaviour
     }
     private void OnDrawGizmos() {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance,wallCheck.position.y , wallCheck.position.z));
     }
 }
