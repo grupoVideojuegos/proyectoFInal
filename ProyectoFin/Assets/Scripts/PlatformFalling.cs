bf86d8ed6dce9d51fa59523992e072a765e842fc
LastSoul <bmsa753@gmail.com>
2020-01-05 02:44:19 -0500

    objeto plataformas que caen y script agregado

---
+++ ProyectoFin/Assets/Scripts/PlatformFalling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFalling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fallDelay = 1.0f;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D bx;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bx = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall",fallDelay);
+        }
+    }
+    void Fall()
+    {
+        rb.isKinematic = false;
+        bx.isTrigger = true;
+    }
+}
