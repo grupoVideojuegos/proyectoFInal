a583bd737756628fe1e88efc475b12918b06a509
LastSoul <bmsa753@gmail.com>
2020-01-05 22:36:06 -0500

    Correccion en transformadas y spawn

--- ProyectoFin/Assets/Scripts/PlatformFalling.cs
+++ ProyectoFin/Assets/Scripts/PlatformFalling.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlatformFalling : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float fallDelay = 1.0f;
+    public float fallDelay = 0.5f;
+    public float respawnDelay = 3.0f;
 
     private Rigidbody2D rb;
     private BoxCollider2D bx;
+    private Vector3 start;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         bx = GetComponent<BoxCollider2D>();
+        start = transform.position;
     }
 
     // Update is called once per frame
@@ -25,11 +28,19 @@ public class PlatformFalling : MonoBehaviour
         if (col.gameObject.CompareTag("Player"))
         {
             Invoke("Fall",fallDelay);
+            Invoke("Respawn", fallDelay + respawnDelay);
         }
     }
     void Fall()
     {
         rb.isKinematic = false;
         bx.isTrigger = true;
+    }
+    void Respawn()
+    {
+        transform.position = start;
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+        bx.isTrigger = false;
     }
 }
